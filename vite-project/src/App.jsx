0230c28d2caf528bf9ce58edbd5f1afa3b4c2c13
Alexander Torres <alexandertorres210408@gmail.com>
2026-03-24 23:31:51 -0500

    Add Home component with movie list and search form

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Home from './pages/Home';
+import MovieCard from './components/MovieCard';
+
+
+function App(){
+  const movieNumber = 2;
+
+
+  return(
+    <>
+      <Home/>
+    </>
+  );
+}
+
+
+
+export default App;
