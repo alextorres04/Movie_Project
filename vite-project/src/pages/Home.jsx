3f25a45c751466366c319909c89963244ec1d177
Alexander Torres <alexandertorres210408@gmail.com>
2026-03-24 23:55:44 -0500

    feat: add search state and controlled input in Home componet

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,24 +1,33 @@
 import MovieCard from "../components/MovieCard";
-
+import { useState } from "react";
 
 
 function Home(){
+    const [searchQuery, setSearchQuery] = useState("");
+
+
     const movies = [
         {id: 1, title: "John Wick", release_date: "2020"},
         {id: 2, title: "Terminator", release_date: "1999"},
         {id: 3, title: "The Matrix", release_date: "1998"},
-    ]
+    ];
 
-    const handleSearch = () => {
-
-    }
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("-------")
+    };
 
     return <div className="home">
         <form onSubmit={handleSearch} className="search-form">
             <input 
                 type="text" 
                 placeholder="Search for movies..." 
-                className="search-input"/>
+                className="search-input"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+                
             <button type="submit" className="search-button"> 
                 Search
             </button>
@@ -27,8 +36,8 @@ function Home(){
 
         <div className="movie-grid">
             {movies.map((movie) => (
-                <MovieCard  movie={movie} key={movie.id}/>
-            ))}
+                <MovieCard  movie={movie} key={movie.id}/>)
+            )}
         </div>
     </div>
 }
